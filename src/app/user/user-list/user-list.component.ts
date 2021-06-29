2196dd55c7aed3fc97baa1d835d18659c2366393
Jeffrey North <JNorth@tql.com>
2021-06-29 16:50:47 -0400

    working

---
+++ src/app/user/user-list/user-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user.class';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: User[] = [];
+  searchCriteria: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
